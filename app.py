513fadffbd26a4f0e6c11f775b1d9f8ad3d93ecc
armindo.guerra <armindo.guerra@ltrace.com.br>
2020-01-08 15:23:21 -0300

    add

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import os
-from flask import Flask, request, render_template
+from flask import Flask, jsonify, request
 
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')#, title='Home', user=user)
+def nao_entre_em_panico():
+    if request.headers.get('Authorization') == '42':
+        return jsonify({"42": "a resposta para a vida, o universo e tudo mais"})
+    return jsonify({"message": "Não entre em pânico!"})
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(port=port)
+    app.run(host='0.0.0.0', port=port)
